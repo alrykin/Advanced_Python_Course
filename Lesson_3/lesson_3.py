15bda38739d07e69071bead2b7ce3f6abc4ca3b8
Aleksandr Yaremenko <everslab@gmail.com>
2019-10-04 21:35:41 +0300

    переделано на словарь

--- Lesson_3/lesson_3.py
+++ Lesson_3/lesson_3.py
@@ -8,21 +8,31 @@ import time
 def decorator(num_of_repeats=1):
     def actual_decorator(func):
         def wrapper(*args, **kwargs):
-            results = []
+            results_in_dict = {}
             for i in range(num_of_repeats):
+                results_in_dict[f"result for iteration {i}"] = {}
                 start_time = time.time()
                 func()
                 func_time = time.time() - start_time
-                result = "function name is: " + func.__name__ + " time for its extcution = " + str(func_time)
-                results.append(result)
+                result = "function name is: " + func.__name__ + ". Time for its extcution = " + str(func_time)
+                results_in_dict[f"result for iteration {i}"]["function name"] = func.__name__
+                results_in_dict[f"result for iteration {i}"]["time for its extcution: "] = str(func_time)
             print("decoration finished")
-            return (results)
+            return results_in_dict
         return wrapper
     return actual_decorator
 
 @decorator(5)
 def some_func():
-    time.sleep(1)
+    # some logic
+    time.sleep(0.2)
 
 
 print(some_func())
+
+
+
+
+##Home Work
+# Создать класс структуры данных Стек, Очередь.
+# Создать класс комплексного числа и реализовать для него арифметические операции.
