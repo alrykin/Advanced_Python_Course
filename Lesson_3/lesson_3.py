17ffd5bf5c91816845e87400a8a5ccdc16d21dee
Aleksandr Yaremenko <everslab@gmail.com>
2019-10-04 22:03:50 +0300

    commit after teacher check of my practical task

--- Lesson_3/lesson_3.py
+++ Lesson_3/lesson_3.py
@@ -2,7 +2,6 @@
 # Создать декоратор с аргументами. Который будет вызывать функцию, определенное кол-во раз,
 # будет выводить кол-во времени затраченного на выполнение данной функции и её название. Practice.
 
-
 import time
 
 def decorator(num_of_repeats=1):
@@ -12,11 +11,12 @@ def decorator(num_of_repeats=1):
             for i in range(num_of_repeats):
                 results_in_dict[f"result for iteration {i}"] = {}
                 start_time = time.time()
-                func()
+                func_results = func()
                 func_time = time.time() - start_time
                 result = "function name is: " + func.__name__ + ". Time for its extcution = " + str(func_time)
                 results_in_dict[f"result for iteration {i}"]["function name"] = func.__name__
                 results_in_dict[f"result for iteration {i}"]["time for its extcution: "] = str(func_time)
+                results_in_dict[f"result for iteration {i}"]["result of  function extcution: "] = func_results
             print("decoration finished")
             return results_in_dict
         return wrapper
@@ -24,9 +24,8 @@ def decorator(num_of_repeats=1):
 
 @decorator(5)
 def some_func():
-    # some logic
     time.sleep(0.2)
-
+    return "VALUE"
 
 print(some_func())
 
@@ -36,3 +35,28 @@ print(some_func())
 ##Home Work
 # Создать класс структуры данных Стек, Очередь.
 # Создать класс комплексного числа и реализовать для него арифметические операции.
+# дополнительное задание: предусмотреть pop при пустом стеке,  ограничить размер стека, _STACK_DICT засунуть в инит
+# class MyStack():
+#     _STACK_DICT = []
+#
+#     def __init__(self):
+#         pass
+#
+#     def push(self, var):
+#         MyStack._STACK_DICT.append(var)
+#
+#     def get_stack(self):
+#         return MyStack._STACK_DICT
+#
+#     def pop(self):
+#         return MyStack._STACK_DICT.pop()
+#
+# stack_test = MyStack()
+# stack_test.push(7)
+# stack_test.push(1)
+# stack_test.push(2)
+# stack_test.push(3)
+# stack_test.push(4)
+# print(stack_test.get_stack())
+# print(stack_test.pop())
+# print(stack_test.get_stack())
