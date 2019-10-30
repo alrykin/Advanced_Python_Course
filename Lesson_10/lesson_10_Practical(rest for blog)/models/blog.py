e926bacc601c50aa983855e4e54c25ffbfd0b684
Aleksandr Yaremenko <everslab@gmail.com>
2019-10-30 22:08:16 +0200

    init commit for lesson 10 practical task

--- Lesson_10/lesson_10_Practical(rest for blog)/models/blog.py
+++ Lesson_10/lesson_10_Practical(rest for blog)/models/blog.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+import datetime
 
 connect("blog_mgdb")
 
@@ -6,15 +7,13 @@ connect("blog_mgdb")
 class Author(Document):
     first_name = StringField(max_length=100)
     last_name = StringField(max_length=100)
-
+    post_count = IntField(default=0)
+    # post_count = Post.objects(author=self).count()
 
 class Post(Document):
     title = StringField(max_length=64)
     post = StringField(max_length=255)
+    view_count = IntField(default=0)
     author = ReferenceField(Author, reverse_delete_rule=1)
+    date = DateTimeField(default=datetime.datetime.now())
     tag = StringField(max_length=20)
-
-
-
-# xcx = Person.objects().distinct('id')
-# print(Person5db9a868cad3393209c323e3' in xcx)
